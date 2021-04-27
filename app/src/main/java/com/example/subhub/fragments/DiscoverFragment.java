f451c3345fa8aad8f32e97c11a25a210eac6aa3f
Ivan Yeung <42223424+RamenWarlord@users.noreply.github.com>
2021-04-27 19:38:06 -0400

    fragments basic

--- app/src/main/java/com/example/subhub/fragments/DiscoverFragment.java
+++ app/src/main/java/com/example/subhub/fragments/DiscoverFragment.java
@@ -5,15 +5,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import com.example.subhub.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link SearchFragment#newInstance} factory method to
+ * Use the {@link DiscoverFragment#newInstance} factory method to
  * create an instance of this fragment.
  *
  */
-public class SearchFragment extends Fragment {
+public class DiscoverFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -30,11 +31,11 @@ public class SearchFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment SearchFragment.
+     * @return A new instance of fragment DiscoverFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static SearchFragment newInstance(String param1, String param2) {
-        SearchFragment fragment = new SearchFragment();
+    public static DiscoverFragment newInstance(String param1, String param2) {
+        DiscoverFragment fragment = new DiscoverFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -42,7 +43,7 @@ public class SearchFragment extends Fragment {
         return fragment;
     }
 
-    public SearchFragment() {
+    public DiscoverFragment() {
         // Required empty public constructor
     }
 
@@ -59,6 +60,6 @@ public class SearchFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        return inflater.inflate(R.layout.fragment_discover, container, false);
     }
 }
